642aa6e857f11cbc65c91e1ceaca67f1884d7792
Baldauren1 <baldaurenzamanov@gmail.com>
2025-11-13 19:31:56 +0500

    adding MSTResult class

---
+++ src/main/java/graph/MSTResult.java
@@ -0,0 +1,20 @@
+package graph;
+
+import java.util.*;
+
+public class MSTResult {
+    public List<Edge> edges;
+    public int totalCost;
+
+    public MSTResult(List<Edge> edges) {
+        this.edges = edges;
+        this.totalCost = edges.stream().mapToInt(Edge::getWeight).sum();
+    }
+
+    public void print() {
+        System.out.println("Edges:");
+        for (Edge e : edges)
+            System.out.println(" " + e);
+        System.out.println("Total cost = " + totalCost);
+    }
+}
